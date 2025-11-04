883db27b164ed4174b547d3c88fcb7f53d4d0941
@0xSCfL <lkhawamed29@gmail.com>
2025-11-04 09:34:59 +0100

    Update the waybar scripts

--- waybar/scripts/spotify-status.sh
+++ waybar/scripts/spotify-status.sh
@@ -2,14 +2,17 @@
 
 if playerctl -l | grep -q "^spotify$"; then
     status=$(playerctl --player=spotify status)
+    artist=$(playerctl --player=spotify metadata artist | sed 's/&/\&amp;/g')
+    title=$(playerctl --player=spotify metadata title | sed 's/&/\&amp;/g')
+
     if [[ "$status" == "Playing" ]]; then
-        artist=$(playerctl --player=spotify metadata artist)
-        title=$(playerctl --player=spotify metadata title)
-        echo "{\"text\":\" $artist - $title\"}"
+        echo "{\"text\":\"  $artist - $title\"}"
+    elif [[ "$status" == "Paused" ]]; then
+        echo "{\"text\":\" $artist - $title\"}"
     else
-        echo "{\"text\":\"\"}"
+        echo "{\"text\":\" \"}"
     fi
 else
-    echo "{\"text\":\"\"}"
+    echo "{\"text\":\" Not running\"}"
 fi
 
