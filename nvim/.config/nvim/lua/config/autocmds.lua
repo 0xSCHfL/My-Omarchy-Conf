038f108d017585ba467d9921f5685f14098c79cf
@0xSCfL <lkhawamed29@gmail.com>
2026-01-21 10:47:23 +0100

    Add ghostty

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,19 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Auto-save session on exit
+vim.api.nvim_create_autocmd("VimLeavePre", {
+  callback = function()
+    require("persistence").save()
+  end,
+})
+
+-- Restore session on startup if no arguments
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    if vim.fn.argc() == 0 then
+      require("persistence").load()
+    end
+  end,
+})
